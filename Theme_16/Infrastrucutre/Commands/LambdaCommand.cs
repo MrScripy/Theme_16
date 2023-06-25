96ddb743a184d5b767b4f528b084e13f78108903
Rina-Scr <muntyanu.kairna@yandex.ru>
2023-06-25 20:32:50 +0300

    Project structure

---
+++ Theme_16/Infrastrucutre/Commands/LambdaCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Theme_16.Infrastrucutre.Commands.Base;
+
+namespace Theme_16.Infrastrucutre.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExchange)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExchange;
+        }
+        public override bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object? parameter) => _execute(parameter);
+    }
+}
