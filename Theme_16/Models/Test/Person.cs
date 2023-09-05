27def19e79155404a8d42fb150f14fdbcad68f1b
Rina-Scr <muntyanu.kairna@yandex.ru>
2023-09-05 21:01:29 +0300

    Fixed data grid style

---
+++ Theme_16/Models/Test/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Theme_16.Models.Test
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Theme_16/Views/Windows/MainWindow.xaml.cs
+++ Theme_16/Views/Windows/MainWindow.xaml.cs
@@ -1,17 +1,37 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using Theme_16.Models.Test;
 using Theme_16.Views;
 
 namespace Theme_16
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Person[] people = new Person[]
+        {
+            new Person
+            {
+                Id= 1,
+                Name = "name",
+                Description = "description"
+            },
+            new Person
+            {
+                Id= 1,
+                Name = "name",
+                Description = "description"
+            },
+            new Person
+            {
+                Id= 1,
+                Name = "name",
+                Description = "description"
+            }
+        };
         public MainWindow()
         {
             InitializeComponent();
+            FirstDG.ItemsSource = people;
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
